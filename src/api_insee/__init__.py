d6d809fd9029e115f2926268c430d9de87f8651b
fsierra <frederic.sierra@a6cmo.fr>
2023-12-08 18:53:34 +0100

    suppress code coverage for unreachable code

--- src/api_insee/__init__.py
+++ src/api_insee/__init__.py
@@ -10,7 +10,7 @@ from .conf import ApiVersion as ApiVersion
 try:
     dist_name = __name__
     __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+except DistributionNotFound:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del get_distribution, DistributionNotFound

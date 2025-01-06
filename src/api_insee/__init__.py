8ff194a0c20d0b002701c64d53951ccd43d83a13
fsierra <frederic.sierra@a6cmo.fr>
2025-01-06 14:36:33 +0100

    add python 3.12 support

--- src/api_insee/__init__.py
+++ src/api_insee/__init__.py
@@ -1,16 +1,3 @@
-from pkg_resources import (
-    DistributionNotFound,
-    get_distribution,
-)
-
 from . import criteria as criteria
 from .api import ApiInsee as ApiInsee
 from .conf import ApiVersion as ApiVersion
-
-try:
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:  # pragma: no cover
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
